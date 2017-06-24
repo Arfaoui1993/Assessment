8af1bb3fddc93f62de5aeeccad5b9c06d767e0d2
Arfaoui1993 <arfaouiaymen1993@gmail.com>
2017-06-24 02:47:14 -0700

    Add ack messages from the gatewayto the backend

--- Assessment/src/gateway.c
+++ Assessment/src/gateway.c
@@ -11,7 +11,7 @@
 # define DELETE_KI					0x0A
 # define OPEN_DOOR					0x0F
 # define ACK_BACKEND					0x11
-# define BREAK_COUNTER					100
+# define GATEWAY_TIMEOUT_COUNTER			100
 
 /**
  * This function is polled by the main loop and should handle any packets coming
@@ -21,12 +21,13 @@ void handle_communication(void)
 {
 	const uint8_t* puint8incomingbackenddata[128];//first byte should contain a command
 	uint8_t uint8sensordata[128];//first byte should contain a command
-	uint8_t uint8backenddatatosend[128];//first byte should contain a command data in here used to be sent to backend
-	uint8_t uint8sensordatatosend[128];//first byte should contain a command data in here used to be sent to sensors
+	uint8_t uint8backenddatatosend[128];//senddata to backend buffer: first byte should contain a command data in here used to be sent to backend
+	uint8_t uint8sensordatatosend[128];//senddata to sensor buffer: first byte should contain a command data in here used to be sent to sensors
 	uint8_t uint8AckBackend = ACK_BACKEND;
 
 	size_t length=128;
-	
+	// Handler should check incoming data from backend and from the sensor, data received from the backend can either go to the gateway or 		// go to the sensor. Data received from the sensor must go back to the backend through the gateway(it can be a pong(after ping), 	 // an acknowledgment,...)
+
 	// Incoming backend data in the gateway state machine 
 	if(modem_dequeue_incoming(puint8incomingbackenddata, &length)){//check any incoming data sockets from the Backend
 		//TODO: fing a  way to deal with wrong incomming data
@@ -40,22 +41,20 @@ void handle_communication(void)
 				}
 			break;
 			case(PING_SENSOR):{
-				modem_enqueue_outgoing(&uint8AckBackend,128);//gateway acknowledge the backend
+				//modem_enqueue_outgoing(&uint8AckBackend,128);//gateway acknowledge the backend//not needed because backend 					//needs sensor ACK
 				for(uint8_t i=0;i<128;i++){
 					uint8sensordatatosend[i]=*puint8incomingbackenddata[i];			
 					}//TODO: find better solution to this
 				wireless_enqueue_outgoing(get_device_id(),uint8sensordatatosend);//gateway ping sensor
-				device_id_t *device_id=NULL;
-				wireless_dequeue_incoming(device_id,uint8sensordata);
-				if( (PING_SENSOR==uint8sensordata[0]) && (NULL!=device_id) ){//gateway receive pong sensor 
-				modem_enqueue_outgoing(uint8sensordata,128);//gateway sends pong senor to backend
-				}
 				}
 			break;
 			case(RESTART_GATEWAY):{
+				//RESTART GATEWAY
 			}
 			break;
 			case(RESTART_SENSOR):{
+				uint8sensordatatosend[0] = RESTART_SENSOR;			
+				wireless_enqueue_outgoing(get_device_id(),uint8sensordatatosend);//Send restart command to sensor
 				}
 			break;
 			case(ADD_NEWKI_TO_SENSOR):{
@@ -69,12 +68,23 @@ void handle_communication(void)
 			break;
 			}
 	}
-		device_id_t *device2_id=NULL;
+		device_id_t *device_id=NULL;
 		// Incoming sensor data in the gateway state machine 
-		if(wireless_dequeue_incoming(device2_id, uint8sensordata)){ //check any incoming data sockets from the Sensor
-				switch(uint8sensordata[0]){
-					
+		// if the gateway don't receive ack from the sensor, it will keep sending data
+		// TODO: Add timeout to receiving data here, if the time out is gone gateway should send the data again
+		if(wireless_dequeue_incoming(device_id, uint8sensordata)){ //check any incoming data sockets from the Sensor
+			if(NULL!=device_id){//check if sensor was detected
+			switch(uint8sensordata[0]){
+				case(PING_SENSOR):{//gateway receive pong sensor 
+				modem_enqueue_outgoing(uint8sensordata,128);//gateway sends the sensor pong to backend
+				}
+				break;
+				case(ACK_BACKEND):{//gateway should should send the ACK of the sensor 
+				modem_enqueue_outgoing(uint8sensordata,1);//gateway sends sensor ACK to backend
+				}
+				break;
 		}
 	}
 }
 
+}
