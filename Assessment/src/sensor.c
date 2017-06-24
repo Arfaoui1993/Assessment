08d6c2535f02af58acc310ace64de21baaa9f830
Arfaoui1993 <arfaouiaymen1993@gmail.com>
2017-06-24 13:04:41 -0700

    Add newki and delete ki commands

--- Assessment/src/sensor.c
+++ Assessment/src/sensor.c
@@ -32,11 +32,11 @@ void handle_communication(void)
 			//sensor should send acknowledgment	
 			wireless_enqueue_outgoing(&uint8AckBackend);//Send Ack to gateway
 			
-			//sensor should receive 4 data sockets now
+			//sensor should receive 4 data sockets now and store them in uint8receivedbackenddata
 				uin8index=0;
 				while(uin8index<128){
-				for(uint8_t i=uin8index;i<32+uin8index;i++){
 				if(wireless_dequeue_incoming(uint8sensorpocketdata)){
+				for(uint8_t i=uin8index;i<32+uin8index;i++){
 				uint8receivedbackenddata[i]=uint8sensorpocketdata[i-uin8index];			
 						}
 				uin8index = uin8index + 32;
@@ -55,6 +55,58 @@ void handle_communication(void)
 				}
 	}
 	break;
-}
-}
+	case(RESTART_SENSOR):{
+	//Resatrt sensor
+	}
+	break;
+	case(ADD_NEWKI_TO_SENSOR):{
+	//sensor should send acknowledgment	
+			wireless_enqueue_outgoing(&uint8AckBackend);//Send Ack to gateway
+	
+				//sensor should receive 4 data sockets now and store them in uint8receivedbackenddata
+				uin8index=0;
+				while(uin8index<128){
+				for(uint8_t i=uin8index;i<32+uin8index;i++){
+				if(wireless_dequeue_incoming(uint8sensorpocketdata)){
+				uint8receivedbackenddata[i]=uint8sensorpocketdata[i-uin8index];			
+						}
+				uin8index = uin8index + 32;
+					}
+					//FIXME: program can bloc in here if there is no data received
+				}
+				uint8_t uint8ki[16];
+				for(uint8_t i=0;i<16;i++){
+					uint8ki[i]=uint8receivedbackenddata[i+8] ;
+				}
+				if(KI_STORE_SUCCESS==ki_store_add(uint8ki)){
+					wireless_enqueue_outgoing(&uint8AckBackend);//Send Ack to gateway
+				}
+	break;
+		}
+			case(DELETE_KI):{
+			//sensor should send acknowledgment	
+			wireless_enqueue_outgoing(&uint8AckBackend);//Send Ack to gateway
+	
+				//sensor should receive 4 data sockets now and store them in uint8receivedbackenddata
+				uin8index=0;
+				while(uin8index<128){
+				for(uint8_t i=uin8index;i<32+uin8index;i++){
+				if(wireless_dequeue_incoming(uint8sensorpocketdata)){
+				uint8receivedbackenddata[i]=uint8sensorpocketdata[i-uin8index];			
+						}
+				uin8index = uin8index + 32;
+					}
+					//FIXME: program can bloc in here if there is no data received
+				}
+				uint8_t uint8ki[16];
+				for(uint8_t i=0;i<16;i++){
+					uint8ki[i]=uint8receivedbackenddata[i+8] ;
+				}
+				if(KI_STORE_SUCCESS==ki_store_remove(uint8ki)){
+					wireless_enqueue_outgoing(&uint8AckBackend);//Send Ack to gateway
+				}
+	break;
+			}
+		}
+	}
 }
